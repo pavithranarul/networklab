fa035a24979e289024fb7aaa6904473b930ab00b
Pavithran Arul <128377410+pavithranarul@users.noreply.github.com>
2023-07-15 22:20:05 +0530

    tcp chat

---
+++ tcp chat/client.py
@@ -0,0 +1,16 @@
+import socket
+def client_program():
+    print('type bye to terminate')
+    host=socket.gethostname()
+    port=5000
+    client_socket=socket.socket()
+    client_socket.connect((host,port))
+    message=input("->")
+
+    while message.lower().strip()!='bye':
+        client_socket.send (message.encode())
+        data=client_socket.recv(1024).decode()
+        print('received from server:'+data)
+        message=input("->")
+    client_socket.close()
+client_program()
